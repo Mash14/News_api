3066cfa6be0db129238471538aca7520c923285b
mash <mashalonzo741@gmail.com>
2021-09-10 11:31:03 +0300

    Add the source class and its test

---
+++ app/models/source.py
@@ -0,0 +1,9 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+    
---
+++ app/source_test.py
@@ -0,0 +1,33 @@
+import unittest
+from models import source
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source('news_tech', 'News Technology')
+
+
+    def test_init(self):
+        '''
+        Method to test if the object is being initialized properly
+        '''
+        self.assertEqual(self.new_source.id,'news_tech')
+        self.assertEqual(self.new_source.name, 'News Technology')
+
+        
+    def test_instance(self):
+        '''
+        Method to test if the object is being initialized
+        '''
+        self.assertTrue(isinstance(self.new_source,Source))
+
+
+if __name__ == '__main__':
+    unittest.main()
