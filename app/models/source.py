d14e3e778d385cbc7cc5256b5eb3f2b0938ece83
mash <mashalonzo741@gmail.com>
2021-09-10 16:29:19 +0300

    Add the article class and its test

--- app/models/source.py
+++ app/models/source.py
@@ -3,7 +3,16 @@ class Source:
     Source class to define Source Objects
     '''
 
+
     def __init__(self,id,name):
+        """
+        __init__method that helps to define properties of our objects
+        
+        Args:
+            id: New source id
+            name: New source name
+        """
+        
         self.id = id
         self.name = name
     
